bce40c47f119c0981e1b65d9edd73f3ba2f8666d
Shanawar <shanawar155@icloud.com>
2025-01-09 23:25:25 +0000

    first commit

---
+++ public/home.php
@@ -0,0 +1,23 @@
+<?php
+
+include ('../templates/header.php');
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astro-Max Home</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="home-background-container">
+        <button class="booknow-btn">BOOK NOW</button>
+    </div>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<header class="navbar">
+    <div class="logo">
+        <a href="http://localhost:3000/public/home.php">
+            <img src="../css/images/logo.png" alt="Astro-Max Logo">
+        </a>
+    </div>
+    <nav class="nav-links">
+        <a href="#" class="nav-link">Home</a>
+        <a href="#" class="nav-link">Activities</a>
+        <a href="#" class="nav-link">Locations</a>
+        <a href="#" class="nav-link">Booking</a>
+    </nav>
+    <div class="login-register">
+        <a href="#" class="login-link">Login | Register</a>
+    </div>
+</header>
