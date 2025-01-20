f05b01eeca0ed1d3c498420a3f34e410065595b9
Shanawar <shanawar155@icloud.com>
2025-01-20 18:35:09 +0000

    DONE

--- config/astro-max.sql
+++ config/astro-max.sql
@@ -11,3 +11,10 @@ CREATE TABLE `activities` (
   `activity_description` varchar(255) NOT NULL,
   `activity_img` varchar(255) NOT NULL
 );
+
+CREATE TABLE `bookings` (
+  `booking_id` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `activity_id` int(10) NOT NULL,
+  `booking_date` date NOT NULL,
+  `booking_time` time NOT NULL
+);
